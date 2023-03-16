0c0452b0b3f898cb6d822c84d955102e4a385cbe
artdev616 <art.drumpian616@gmail.com>
2023-03-16 14:13:23 -0400

    (bug): fix the cors issue

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,10 @@ const router = require("./routes");
 
 const app = express();
 
-// use router
-app.use('/api', router);
-
 app.use(cors());
 app.use(express.json());
+
+// use router
+app.use('/api', router);
 
 module.exports = app;
